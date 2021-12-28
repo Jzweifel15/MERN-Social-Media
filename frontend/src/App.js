96e8531131ce19b264de8d027838524b96ba4091
Justin R. Zweifel <jzweifel15@yahoo.com>
2021-12-28 18:16:56 -0500

    Tweaking
    Tweaked the main App component to make the code more consistent

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import { getPosts } from "./Store/Actions/posts";
 import memories from "./images/memories.png";
 
 const App = () => {
-  const [currentId, setCurrentId] = useState(null);
+  const [currentId, setCurrentId] = useState(0);
   const classes = useStyles();
   const dispatch = useDispatch();
 
@@ -18,17 +18,17 @@ const App = () => {
 
   return (
     <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-        <img className={classes.image} src={memories} alt="memories" height="60" />
+      <AppBar className={ classes.appBar } position="static" color="inherit">
+        <Typography className={ classes.heading } variant="h2" align="center">Memories</Typography>
+        <img className={ classes.image } src={ memories } alt="memories" height="60" />
       </AppBar>
       <Grow in>
         <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
+          <Grid container justify="space-between" alignItems="stretch" spacing={ 3 }>
+            <Grid item xs={ 12 } sm={ 7 }>
               <Posts setCurrentId={ setCurrentId } />
             </Grid>
-            <Grid item xs={12} sm={4}>
+            <Grid item xs={ 12 } sm={ 4 }>
               <Form currentId={ currentId } setCurrentId={ setCurrentId } />
             </Grid>
           </Grid>
